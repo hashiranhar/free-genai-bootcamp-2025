7726577852a91f21e833ad56daf95792ea73413a
hashiranhar <hashiranhar2021@gmail.com>
2025-03-27 13:48:01 +0000

    #4 remove old javascript

--- lang-portal/frontend-react/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use the new ReactDOM API
-import App from './App';
-import './styles/global.css'; // Import global styles
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
-root.render(<App />); // Render the App component
